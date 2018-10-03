ccd81a6b6d13d5d7366a2d5aad8f5c9e9cdcba8c
dmrib <dmrib.cs@gmail.com>
2018-10-03 08:08:52 -0300

    Client connection to server

---
+++ hotline/advanced/client.py
@@ -0,0 +1,29 @@
+import json
+from twisted.internet import reactor, protocol
+
+
+class EchoClient(protocol.Protocol):
+    def connectionMade(self):
+        self.transport.write('{"command" : "hangup", "id": "4"}'.encode('utf-8'))
+
+    def dataReceived(self, data):
+        message = json.loads(data.decode('utf-8'))
+        print(message['message'])
+        self.transport.loseConnection()
+
+
+class EchoFactory(protocol.ClientFactory):
+    def buildProtocol(self, addr):
+        return EchoClient()
+
+    def clientConnectionFailed(self, connector, reason):
+        print("Connection failed.")
+        reactor.stop()
+
+    def clientConnectionLost(self, connector, reason):
+        print("Connection lost.")
+        reactor.stop()
+        
+
+reactor.connectTCP("localhost", 5678, EchoFactory())
+reactor.run()
